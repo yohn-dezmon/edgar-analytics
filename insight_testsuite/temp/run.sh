53dc9bf534c6ec6fe15c8c0c2739742cd4d167d4
Johnboi <johndesmond631@gmail.com>
2020-02-21 15:07:54 -0500

    passed basic unit test

--- insight_testsuite/temp/run.sh
+++ insight_testsuite/temp/run.sh
@@ -3,6 +3,17 @@
 # Use this shell script to compile (if necessary) your code and then execute it. Below is an example of what might be found in this file if your program was written in Python
 #
 #python ./src/sessionization.py ./input/log.csv ./input/inactivity_period.txt ./output/sessionization.txt
+workdir=$(pwd)
+# This is useful to distinguish betweeen test and non-test runs.
+parentdir="$(dirname "$(pwd)")"
+echo $workdir
+echo $parentdir
+inputfile="/input/log.csv"
+outputfile="/output/sessionization.txt"
+concat_path="$workdir$inputfile"
+concat_path_out="$workdir$outputfile"
+lastLine=$(tail -n 1 "$concat_path")
+
 
 
 # cd src
